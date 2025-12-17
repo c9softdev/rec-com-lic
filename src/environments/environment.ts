f60a3bdd1fd3da9436d7e56460b78457ecdbcd47
c9softdev <admin@c9soft.com>
2025-12-17 17:04:15 +0530

    NABDH_SETTING

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://c9soft.com/api/rec/portal/',
+  // apiUrl: 'https://nabdhtech.com/api/portal/',
   apiUrl: 'https://c9soft.com/api/recLicense/portal/',
   apiUrlLic: 'https://c9soft.com/api/master/portal/',
   // sec_key: ""
   comp_id: "", // Default company ID for config fetch before session is established
-  superAdminID: "999", 
+  // superAdminID: "999", 
+  superAdminID: "9999", 
   recaptchaSiteKey: '6LfzlvgrAAAAAMyEu8bfdcgXJsGw_E_5Jk8rUYJb'
 };
